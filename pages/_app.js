58df3bc5df69b5fa616fb8fccdbff867e542da4e
Coda Bool <CodaBool@pm.me>
2020-12-21 18:40:34 -0500

    better footer

--- pages/_app.js
+++ pages/_app.js
@@ -28,19 +28,19 @@ export default function MyApp({ Component, pageProps }) {
   }
 
   return (
-    <>
+    <div className="site">
       <Head>
         <title>Coda Bool</title>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.gif" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.gif" />
       </Head>
       <Navigation />
-      <main>
+      <main className="site-content">
         <Container>
           <Component {...pageProps} />
         </Container>
       </main>
       <Footer />
-    </>
+    </div>
   )
 }
